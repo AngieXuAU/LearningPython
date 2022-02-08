64e7b160f2b1b3d4fb44002a7ea8a3bf71b9b8ac
AngieXuAU <angelinaxuwenlan@gmail.com>
2022-02-09 07:19:41 +1100

    Import template programs from school to Git.

---
+++ python_school_exercise_files/Ch2/helloworld_start.py
@@ -0,0 +1,21 @@
+#
+# Example file for HelloWorld
+#
+
+#print basic hello world
+print("Hello World")
+
+
+#basic interactive code
+name = input("What is your name? ")
+print("Nice to meet you", name)
+
+
+#defining a function to encompass above code
+def main():
+    print("Hello World, I hereby acknowledge that C++ is clearly superior")
+    name = input("What is your name? ")
+    print("Nice to meet you", name)
+
+if __name__ == "__main__":
+    main()
